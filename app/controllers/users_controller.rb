a95c0cd1f69869fe9cc839ddca5ca5d64cfc725c
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-03-29 20:50:34 +0900

    edit actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
 
   def show
+    @user = User.gind(params[:id])
   end
   
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    current_user.update(update_params)
+    redirect_to controller: "prototype", action: "index"
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :group, :profile, :carrier, :avatar)
   end
 end
