102f8869ff43524ce3b2ee3359689299a8d443cf
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-03-29 20:39:57 +0900

    set required actions

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,2 +1,40 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only:[:edit, :update, :show]
+
+  def index
+    @prototypes = Prototype.order("created_at DESC").page(params[:page]).per(8)
+  end
+
+  def new
+    @prototype = Prototype.new
+    @prototype.upload_images.build
+  end
+
+  def create
+    @prototype = current_user.prototypes.new(create_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  private
+    def create_params
+      params.require(:prototype).permit(:title, :catchcopy, :concept, :user_id)
+    end
+
+    def set_prototype
+      @prototype = Prototype.find(params[:id])
+    end
 end
