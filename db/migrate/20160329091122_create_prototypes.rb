c2bc7cc7aae556a9c2b13ead6706f977ae876c18
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-03-29 18:18:47 +0900

    set prototype table structure

--- db/migrate/20160329091122_create_prototypes.rb
+++ db/migrate/20160329091122_create_prototypes.rb
@@ -1,7 +1,10 @@
 class CreatePrototypes < ActiveRecord::Migration
   def change
     create_table :prototypes do |t|
-
+      t.string :title
+      t.text :catthcopy
+      t.text :concept
+      t.integer :user_id
       t.timestamps null: false
     end
   end
