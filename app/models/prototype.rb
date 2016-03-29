cd275504622e5f72586b76ef9d19d62d101b9b4f
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-03-29 21:38:11 +0900

    modify models

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,8 +1,3 @@
 class Prototype < ActiveRecord::Base
   has_many :upload_images
-  enum status: [:main, :sub]
-
-  mount_uploader :name, UploadImageUploader
-
-  validates_presence_of :name
 end
--- app/models/upload_image.rb
+++ app/models/upload_image.rb
@@ -1,3 +1,9 @@
 class UploadImage < ActiveRecord::Base
-  belongs_to :prototype
+  belongs_to :prototype, dependent: :destroy
+
+  enum status: [:main, :sub]
+
+  mount_uploader :name, UploadImageUploader
+
+  validates_presence_of :name
 end
