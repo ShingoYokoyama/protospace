6b354e207223c7b04f2c45ff8c97ffe06df88446
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-04-03 19:59:22 +0900

    add edit, update, and destroy actions to prototypes_controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-
+before_action :set_prototype, only: [:edit, :update]
 
   def index
     @prototypes = Prototype.order(created_at: :DESC)
@@ -19,9 +19,29 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototypes_params)
+      redirect_to root_path
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path
+  end
+
   private
     def prototypes_params
       params.require(:prototype).permit(:title, :catchcopy, :concept)
+    end
+
+    def set_prototype
+      @prototype = Prototype.find(params[:id])
     end
 
 end
