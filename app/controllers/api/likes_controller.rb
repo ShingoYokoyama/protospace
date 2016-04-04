88de0c416ade57f256bde1a44e05edf95860029b
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-04-04 19:10:31 +0900

    generate likes_controller

---
+++ app/controllers/api/likes_controller.rb
@@ -0,0 +1,22 @@
+class LikesController < ApplicationController
+    before_action :set_like, only: :destroy
+
+  def create
+    @like = Like.create(create_params)
+    @prototype  = @like.prototype
+  end
+
+  def destroy
+    @like.destroy
+    @prototype = Prototype.find(@like.prototype_id)
+  end
+
+  private
+    def create_params
+      params.permit(:prototype_id).merge(user_id: current_user.id)
+    end
+
+    def set_like
+      @like = Like.find_by(user_id: current_user.id, prototype_id: params[:id])
+    end
+end
