35366bf9d647f6f4188d0521ca22dfabfa64718e
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-04-04 18:22:11 +0900

    generate like model and set association among user, prototype and like models

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,6 @@
 class Prototype < ActiveRecord::Base
   has_many :upload_images, dependent: :destroy
+  has_many :likes, dependent: :destroy
   belongs_to :user
   accepts_nested_attributes_for :upload_images
   validates_presence_of :title, :catthcopy, :concept
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403161002) do
+ActiveRecord::Schema.define(version: 20160404091232) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "prototypes", force: :cascade do |t|
     t.string   "title",      limit: 255
