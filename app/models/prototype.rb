42a492ae82113fe77ae4aa6b83961f470354efd4
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-04-09 02:26:34 +0900

    modify code

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :upload_images, dependent: :destroy
+  
   accepts_nested_attributes_for :upload_images
 
   def main_image
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :prototypes, only: [:index, :new, :create, :edit, :update, :destroy]
+  root "prototypes#index"
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  root "prototypes#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
