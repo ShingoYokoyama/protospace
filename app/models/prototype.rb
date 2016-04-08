d0dbe7f5a1e6a1bf469eb4a82060d045cb6489c4
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-04-09 00:19:37 +0900

    modify code and create partial template of file_fields

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,3 +1,13 @@
 class Prototype < ActiveRecord::Base
+  belongs_to :user
   has_many :upload_images, dependent: :destroy
+  accepts_nested_attributes_for :upload_images
+
+  def main_image
+    upload_images.main.first
+  end
+
+  def sub_images
+    upload_images.sub
+  end
 end
