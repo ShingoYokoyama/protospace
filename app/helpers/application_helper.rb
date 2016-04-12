89c175588c2642215e781e667042fcf1b50023e0
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-04-12 10:15:17 +0900

    modify code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,3 @@
 module ApplicationHelper
-  def main_image
-    @prototype.upload_images.main.first
-  end
-
-  def sub_images
-    @prototype.upload_images.sub
-  end
-
-  def prototype_user
-    @protype.user
-  end
-
-  def prototype_images
-    @prototype.upload_images
-  end
-
-  def user_like_exists?
-    @prototype.user_like(current_user) ? true : false
-  end
-
+ 
 end
