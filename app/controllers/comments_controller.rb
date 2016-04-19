ca5475e2abb378b7df934ff560fed9183b4b7108
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-04-20 01:38:02 +0900

    edit controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.create(comment_params)
+    redirect_to :back
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text, :user_id, :prototype_id).merge(user_id: current_user.id)
   end
 end
