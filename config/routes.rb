00325d4646d77e55b4e7bcfe9dca050cca15991b
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-04-19 15:05:15 +0900

    set rooting of create and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :prototypes do
     resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
   root "prototypes#index"
   devise_for :users
