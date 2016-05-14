ffd545edbb00896bc82078a6c8edd7b6c024a7e2
ShingoYokoyama <syyy.nyoshi@gmail.com>
2016-05-15 01:08:21 +0900

    modify

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'us-east-1'
+    region: 'ap-northeast-1'
   }
 
     case Rails.env
